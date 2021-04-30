e57564d6d1e8f665abfc756380d772fe8fa8c2d4
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-04-30 11:07:23 +0200

    added 'UsePowerUp' function, however, it does nothing for now. Added private bool 'hasPowerUp' and 'RotateFirePoint' function, which will rotate the FirePoint if the Player has moved his Player

--- Assets/PowerUpsScripts/Bullet.cs
+++ Assets/PowerUpsScripts/Bullet.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public Rigidbody2D rB2D;
+    public float bulletSpeed = 18f;
+    private Vector3 bulletPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rB2D.velocity = transform.right * bulletSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        
+        if (OutOfBounds() != new Vector3(0,0))
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        Debug.Log(collider);
+        Destroy(gameObject);
+    }
+
+    private Vector3 OutOfBounds()
+    {
+        if(transform.position.x >= 17.36)
+        {
+            bulletPosition = new Vector3(-transform.position.x + 0.1f, transform.position.y);
+            return bulletPosition;
+        }
+        else if (transform.position.x <= -17.36)
+        {
+            bulletPosition = new Vector3(-transform.position.x - 0.1f, transform.position.y);
+            return bulletPosition;
+        }
+        else if (transform.position.y >= 13.4)
+        {
+            bulletPosition = new Vector3(transform.position.x, -11.3099f);
+            return bulletPosition;
+        }
+        else if (transform.position.y <= -11.3199)
+        {
+            bulletPosition = new Vector3(transform.position.x, 13.39f);
+            return bulletPosition;
+        }
+        bulletPosition = new Vector3(0f, 0f);
+        return bulletPosition;
+    }
+
 }
