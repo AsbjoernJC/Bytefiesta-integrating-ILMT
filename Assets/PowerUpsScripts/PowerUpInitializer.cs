06decc37f155b8763c6382cd5b1a55a81b11d3a7
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-04-30 14:58:10 +0200

    Successfully spawns the KingoftheHill0 PowerUpPrefab added 'Todo'

---
+++ Assets/PowerUpsScripts/PowerUpInitializer.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PowerUpInitializer : MonoBehaviour
+{
+    public float duration = 4f;
+    public GameObject[] powerUps; 
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+        SpawnPowerUp(powerUps[Random.Range(0, powerUps.Length)]);
+    }
+
+
+// There should only be spawned a powerUp from powerUps if there is no object with the "PowerUp" tag in the scene
+
+// There needs to be a duration between how long before the next power up can be spawned from when it is picked up
+    private void SpawnPowerUp(GameObject powerUp) 
+    {
+        Instantiate(powerUp);
+    }
+}
