dc5c7c3a9601ced0c8ca7cc7c15b6f792b3cf31d
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-05-04 15:35:26 +0200

    added todo. Work on adding the powerups to the player it collides with

--- Assets/PowerUpInitializerScripts/PowerUp0.cs
+++ Assets/PowerUpInitializerScripts/PowerUp0.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PowerUp0 : MonoBehaviour
 {
+
     // Start is called before the first frame update
-    void Start()
+
+    void Update() 
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+// Todo: The bullet should not be able to collide with the player who shot the bullet
+// Should somehow add the picked up powerup to the player, who it collided with.
+    private void OnTriggerEnter2D(Collider2D collider) 
     {
+        string Collision = collider.ToString();
+        Debug.Log(Collision);
+        if (!Collision.Contains("KingoftheHill0"))
+            Destroy(gameObject);
+        // if (!Collision.Contains("Player 1") && !Collision.Contains("KingoftheHill0"))
+        //     Destroy(gameObject);
         
     }
 }
