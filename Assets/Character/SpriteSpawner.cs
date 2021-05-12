c6c728d6999d252cfde26d45af2822299d904141
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-05-12 10:19:36 +0200

    sprites are now shown properly so players won't be confused that they lost one of their bullets on picking up a shield powerup

--- Assets/Character/SpriteSpawner.cs
+++ Assets/Character/SpriteSpawner.cs
@@ -10,20 +10,35 @@ public class SpriteSpawner : MonoBehaviour
 
     [SerializeField]
     private Sprite[] sprites;
+    private int bulletSpriteCount;
+    private int shieldSpriteCount;
 
 
     // should not be assigned to spriteLocations[0] as it could override a useable shield powerup slot.
     public void SpawnBulletSprites(int bulletCounter)
     {
-        for (int i = 0; i < bulletCounter; i++)
+        if (shieldSpriteCount == 1)
         {
-            spriteLocations[i].sprite = sprites[0];
+            for (int i = 0; i < bulletCounter; i++)
+            {
+                spriteLocations[i].sprite = sprites[0];
+            }
+            bulletSpriteCount = 3;
+        }
+        else
+        {
+            for (int i = 0; i < bulletCounter; i++)
+            {
+                spriteLocations[i].sprite = sprites[0];
+            }
+            bulletSpriteCount = 3;
         }
     }
 
     public void RemoveBulletSprite(int bulletCounter)
     {
         spriteLocations[bulletCounter].sprite = null;
+        bulletSpriteCount --;
     }
 
 
@@ -32,10 +47,16 @@ public class SpriteSpawner : MonoBehaviour
     public void SpawnShieldSprite()
     {
         spriteLocations[0].sprite = sprites[1];
+        shieldSpriteCount = 1;
     }
 
     public void RemoveSprite()
     {
         spriteLocations[0].sprite = null;
+        if (bulletSpriteCount == 3)
+        {
+            spriteLocations[0].sprite = sprites[0];
+        }
+        shieldSpriteCount --;
     }
 }
