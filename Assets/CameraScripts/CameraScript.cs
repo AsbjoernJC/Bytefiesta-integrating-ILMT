39892eb196e1555abfa068c8d76958629e6f1122
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-05-24 11:58:19 +0200

    changed Śtart() to Awake()

--- Assets/CameraScripts/CameraScript.cs
+++ Assets/CameraScripts/CameraScript.cs
@@ -13,7 +13,7 @@ public class CameraScript : MonoBehaviour
     public static float deltaRatio;
     public SpriteRenderer Background;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         currentWidth = Screen.width;
         currentHeight = Screen.height;
