3bfe788be25b20114c2f8e16732c140fe297be04
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-05-25 14:15:39 +0200

    when players join their controllerID ie. when using an xbox controller players are assigned numbers eg. 1, 2, 3 or 4. This lets players know, who controlls who in the newcharacterselect.

--- Assets/MenuScripts/ControllerImageAndText.cs
+++ Assets/MenuScripts/ControllerImageAndText.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ControllerImageAndText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    public TMP_Text[] controllerText;
+    [SerializeField]
+    public Image[] controllerPictures;
+
+    public void ChangeControllerText(string controllerID, int playerIndex)
     {
-        
+        controllerPictures[playerIndex].enabled = true;
+        controllerText[playerIndex].text = controllerID;
+        // controllerText 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
