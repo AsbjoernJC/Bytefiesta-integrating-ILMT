86ff7e379a393b391c11ac15b336e1a2109be379
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-05-26 16:16:21 +0200

    When a player wins it will display an image, however, it should also show the correct player sprite

--- Assets/Scoretracking/KingoftheHill/KingoftheHillTracker.cs
+++ Assets/Scoretracking/KingoftheHill/KingoftheHillTracker.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class KingoftheHillTracker : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas minigameEndImagery;
+    [SerializeField]
+    public Sprite[] playerSprites;
     public static Dictionary<string, int> playerScores = new Dictionary<string, int>()
     {
         {"Player 1", 0},
@@ -22,13 +27,25 @@ public class KingoftheHillTracker : MonoBehaviour
             instance = this;
     }
 
-    public void MiniGameEnd()
+    public static void MiniGameEnd(string playerWhoWon)
     {
+        Time.timeScale = 0f;
+        Debug.Log("Minigame ended");
+        instance.StartCoroutine("DisplayWinTimer");
         // If a player has a score equal to or higher than 5.
         // Here we should load a scene that displays the amount of sips a player should drink
         // After displaying that and or mystery shot it should display the amount of minigame wins a player has
         // SceneManager.LoadScene("KingoftheHill2304");
-        // DifficultyAndScore.finishedMinigames ++;
+        DifficultyAndScore.finishedMinigames ++;
+    }
+
+    private IEnumerator DisplayWinTimer()
+    {
+        minigameEndImagery.gameObject.SetActive(true);
+        yield return new WaitForSecondsRealtime(3.5f);
+        Debug.Log("Waited 3.5 seconds");
+        // To do: load some scene that displays how many sips a player should drink
     }
 
 }
+
