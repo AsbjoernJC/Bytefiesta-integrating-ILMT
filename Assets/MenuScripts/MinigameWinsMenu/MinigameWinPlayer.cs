bbdf5941fac12e77e8efb30eaa81b0b7697f4173
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-17 15:41:12 +0200

    issue was fixed by not using Input System UI Input Module with multiplayer event system and instead sending unity events directly from the player's controlled menu triggering the method in gameinstructioncontroller or in one of it's children depending on the scene. We manually 'activate' the button through this method and lists the player as ready

--- Assets/MenuScripts/MinigameWinsMenu/MinigameWinPlayer.cs
+++ Assets/MenuScripts/MinigameWinsMenu/MinigameWinPlayer.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MinigameWinPlayer : GameInstructionsController
 {
     public override void SetPlayerReady()
     {
+        if (hasPressedReady)
+            return;
+
         readyButton.gameObject.SetActive(false);
         pressedReadyButton.gameObject.SetActive(true);
         MinigameWinManager.Instance.numberOfReadyPlayers ++;
+        
+        hasPressedReady = true;
     }
 }
--- Assets/MenuScripts/MysteryDrinkMenu/MysteryDrinkPlayer.cs
+++ Assets/MenuScripts/MysteryDrinkMenu/MysteryDrinkPlayer.cs
@@ -7,9 +7,14 @@ public class MysteryDrinkPlayer : GameInstructionsController
 {
     public override void SetPlayerReady()
     {
+        if (hasPressedReady)
+            return;
+
         readyButton.gameObject.SetActive(false);
         pressedReadyButton.gameObject.SetActive(true);
         MysteryDrinkManager.numberOfReadyPlayers ++;
+        
+        hasPressedReady = true;
     }
 
 }
