6821f1f32e2ca5e988d557e43914b2529426edd0
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-23 15:44:53 +0200

    added in a way to finish the minigame

---
+++ Assets/Resources/TrapRunScripts/FinishLine.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string collisionTag;
+    private GameObject player;
+    private bool hasCollided = false;
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        string collision = collider.ToString();
+        collisionTag = collider.tag;
+
+        // Anything that isn't a player
+        if (collision.Contains("KingoftheHill") || collision.Contains(this.name) ||collision.Contains("Cannon"))
+            return;
+
+        // A player's head detection is a child of the player prefab therefore we need to get the gameobject of the parent
+        if (collision.Contains("HeadDetect") || collision.Contains("Feet"))
+            player = collider.transform.parent.gameObject;
+        // If the collider still belongs to the player it is directly a part of the player GameObject/prefab
+        else
+            player = collider.gameObject;
+        
+
+
+
+        if (collisionTag.Contains("Player") && hasCollided == false)
+        {
+            // Todo log winner and playerstandings. Maybe based on how many times the players died
+            SceneManager.LoadScene("MinigameWinsMenu");
+        }
+        Destroy(gameObject);
+    }
+}
