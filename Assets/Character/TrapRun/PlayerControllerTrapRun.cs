c50d0ebf8507e1e9f881da7eec8397a21df279e7
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-23 12:16:51 +0200

    added line which should allow players to pass through one another

---
+++ Assets/Character/TrapRun/PlayerControllerTrapRun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTrapRun : PlayerController
+{
+    // Start is called before the first frame update
+    protected override void Awake() 
+    {
+        base.Awake();
+        Physics.IgnoreLayerCollision(6, 6);
+    }
+
+}
