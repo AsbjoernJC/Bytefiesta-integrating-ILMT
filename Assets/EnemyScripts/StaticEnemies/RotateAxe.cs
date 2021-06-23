2ddd6e95acd30b73ce7987ab46315109527e6f3a
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-23 14:03:38 +0200

    added script that will rotate an axe around a transform coordinate/position ie. (15, -10, 0)

---
+++ Assets/EnemyScripts/StaticEnemies/RotateAxe.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAxe : MonoBehaviour
+{
+    [SerializeField] public Transform rotationPoint;
+
+    // rotationSpeed is in units of angle (degrees) / second
+    [SerializeField] public float rotationSpeed = 40f;
+    private Transform axeTransform;
+    private void Awake()
+    {
+        axeTransform = this.gameObject.transform;
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        axeTransform.RotateAround(rotationPoint.position, new Vector3(0f, 0f, 1f), rotationSpeed * Time.deltaTime);
+    }
+}
