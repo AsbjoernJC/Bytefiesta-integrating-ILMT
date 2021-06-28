1dbf73875db455652e1dee815c082fb92d174513
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-28 15:42:01 +0200

    reorganized code

--- Assets/LevelScripts/DropDead/FallingPlatformManager.cs
+++ Assets/LevelScripts/DropDead/FallingPlatformManager.cs
@@ -9,6 +9,12 @@ public class FallingPlatformManager : MonoBehaviour
     [SerializeField] private Transform[] platformSpawnpoints;
     [SerializeField] private GameObject fallingPlatformPrefab;
 
+    [SerializeField] public GameObject startingPlatform;
+
+    private bool startedCountdown = false;
+
+    private int lastChosenSpawnpoint;
+    private int spawnCounter;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +24,129 @@ public class FallingPlatformManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.timeScale != 0 && !startedCountdown)
+        {
+            StartCoroutine("Countdown");
+            startedCountdown = true;
+        }
     }
 
-    private void ChooseSpawnpoint()
+
+    private IEnumerator Countdown()
     {
+        StartCoroutine("ChooseSpawnpoint");
+        yield return new WaitForSeconds(4);
+        Destroy(startingPlatform);
+    }
+
+
+    private IEnumerator ChooseSpawnpoint()
+    {
+        int chosenSpawnpoint;
+
+        if (spawnCounter == 0)
+        {
+            chosenSpawnpoint = Random.Range(0, 5);
+            lastChosenSpawnpoint = chosenSpawnpoint;
+            spawnCounter ++;
+            SpawnPlatform(chosenSpawnpoint, 3);
+            yield return new WaitForSeconds(2);
+        }
+
+
+        while (LastManStanding.instance.deadPlayers < PlayerConfigurationManager.Instance.numberOfActivePlayers -1)
+        {
+            switch (lastChosenSpawnpoint)
+            {
         // The largest possible horizontal reach of a player is eg. from platformSpawnpoints[0] to platformSpawnpoints[3]
+                case 0:
+                // might have to change if players will be allowed to through the sides of the screen
+                    chosenSpawnpoint = Random.Range(0, 4);
+                    lastChosenSpawnpoint = chosenSpawnpoint;
+                    SpawnPlatform(chosenSpawnpoint, 3f);
+                    spawnCounter ++;
+                    yield return new WaitForSeconds(2);
+                    break;
+                case 1:      
+                    chosenSpawnpoint = Random.Range(0, 5);
+                    lastChosenSpawnpoint = chosenSpawnpoint;
+                    SpawnPlatform(chosenSpawnpoint, 3f);
+                    spawnCounter ++;
+                    yield return new WaitForSeconds(2);
+                    break;
+                case 2:
+                    chosenSpawnpoint = Random.Range(0, 5);
+                    lastChosenSpawnpoint = chosenSpawnpoint;
+                    SpawnPlatform(chosenSpawnpoint, 3f);
+                    spawnCounter ++;
+                    yield return new WaitForSeconds(2);
+                    break;
+                case 3:
+                    chosenSpawnpoint = Random.Range(0, 5);
+                    lastChosenSpawnpoint = chosenSpawnpoint;
+                    SpawnPlatform(chosenSpawnpoint, 3f);
+                    spawnCounter ++;
+                    yield return new WaitForSeconds(2);
+                    break;
+                case 4:
+                    chosenSpawnpoint = Random.Range(1, 5);
+                    lastChosenSpawnpoint = chosenSpawnpoint;
+                    SpawnPlatform(chosenSpawnpoint, 3f);
+                    spawnCounter ++;
+                    yield return new WaitForSeconds(2);
+                    break;
+            }
+        }
+        // for testing:
+        // while (true)
+        // {
+
+        //     switch (lastChosenSpawnpoint)
+        //     {
+        //         case 0:
+        //         // might have to change if players will be allowed to through the sides of the screen
+        //             chosenSpawnpoint = Random.Range(0, 4);
+        //             lastChosenSpawnpoint = chosenSpawnpoint;
+        //             SpawnPlatform(chosenSpawnpoint, 3f);
+        //             spawnCounter ++;
+        //             yield return new WaitForSeconds(2);
+        //             break;
+        //         case 1:      
+        //             chosenSpawnpoint = Random.Range(0, 5);
+        //             lastChosenSpawnpoint = chosenSpawnpoint;
+        //             SpawnPlatform(chosenSpawnpoint, 3f);
+        //             spawnCounter ++;
+        //             yield return new WaitForSeconds(2);
+        //             break;
+        //         case 2:
+        //             chosenSpawnpoint = Random.Range(0, 5);
+        //             lastChosenSpawnpoint = chosenSpawnpoint;
+        //             SpawnPlatform(chosenSpawnpoint, 3f);
+        //             spawnCounter ++;
+        //             yield return new WaitForSeconds(2);
+        //             break;
+        //         case 3:
+        //             chosenSpawnpoint = Random.Range(0, 5);
+        //             lastChosenSpawnpoint = chosenSpawnpoint;
+        //             SpawnPlatform(chosenSpawnpoint, 3f);
+        //             spawnCounter ++;
+        //             yield return new WaitForSeconds(2);
+        //             break;
+        //         case 4:
+        //             chosenSpawnpoint = Random.Range(1, 5);
+        //             lastChosenSpawnpoint = chosenSpawnpoint;
+        //             SpawnPlatform(chosenSpawnpoint, 3f);
+        //             spawnCounter ++;
+        //             yield return new WaitForSeconds(2);
+        //             break;
+        //     }
+
+        // }
     }
 
-    private void SpawnPlatform()
+    private void SpawnPlatform(int chosenSpawnpoint, float fallSpeed)
     {
-        GameObject fallingPlatform = Instantiate(fallingPlatformPrefab);
+        GameObject fallingPlatform = Instantiate(fallingPlatformPrefab, platformSpawnpoints[chosenSpawnpoint]);
+        fallingPlatform.GetComponent<FallingPlatform>().StartFalling(3);
     }
 }
