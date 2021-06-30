49523186feb02ffd556f6113aa0a295165728105
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-06-30 09:38:32 +0200

    ./levelscripts/JumpTheGun/*.cs files are not ignored anymore

---
+++ Assets/LevelScripts/JumpTheGun/TargettingInformation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargettingInformation : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        
+    }
+}
