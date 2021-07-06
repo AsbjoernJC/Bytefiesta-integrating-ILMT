3a23895c32ee4673c727f5b4e668108046b6ce0b
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-07-06 15:38:48 +0200

    added a class that contains the player's information in jumpthegun: do they control the cannon, which team are they on and their player index. added functions that assigns the players their roles and teams: do they control the cannon or do they control the player who has to do the platforming.

--- Assets/LevelScripts/JumpTheGun/JumpTheGunLevelInitializer.cs
+++ Assets/LevelScripts/JumpTheGun/JumpTheGunLevelInitializer.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Users;
 using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class JumpTheGunLevelInitializer : LevelInitializer
 {
     [SerializeField] Transform[] playerCannonSpawns;
+    [SerializeField] GameObject[] playerCannonPrefab;
 
+    private List<JumpTheGunPlayer> team1PlayerData = new List<JumpTheGunPlayer>();
+    private List<JumpTheGunPlayer> team2PlayerData = new List<JumpTheGunPlayer>();
+    private Dictionary<int, JumpTheGunPlayer> teamPlayerData = new Dictionary<int, JumpTheGunPlayer>();
+    private int team1Counter = 0;
+    private int team2Counter = 0;
+
+
+    // where int is playerIndex and the bool is whether or not the player's role is as a player or cannon controlling player
 
     void Awake() 
     {
@@ -24,25 +34,50 @@ public class JumpTheGunLevelInitializer : LevelInitializer
     {
         for (int i = 0; i < PlayerConfigurationManager.Instance.playerControllers.Count; i++)
         {
+            AssignTeam(i);
+            // SpawnPlayer(i);
+            // should respawn players and interact with powerupinitializer
+            
+        }
+
+        for (int i = 0; i < PlayerConfigurationManager.Instance.playerControllers.Count; i++)
+        {
             SpawnPlayer(i);
             // should respawn players and interact with powerupinitializer
             
-            if (LevelInitializer.levelRules[sceneName]["hasScoreUI"])
-                InstantiatePlayerUI(i);
         }
 
     }
     
 
     // Instantiates the player in the current scene
-    public override void SpawnPlayer(int playerIndex, string playerTag)
+    public override void SpawnPlayer(int playerIndex)
     {
         var playerController = PlayerConfigurationManager.Instance.playerControllers[playerIndex];
         var playerControlScheme = PlayerConfigurationManager.Instance.playerControlSchemes[playerIndex];
 
+
+
+        // Checking which team the player belongs to and whether or not they control a cannon or a player
+        var playerTeam = teamPlayerData[playerIndex].team;
+        var hasCannonControl = teamPlayerData[playerIndex].controlsCannon;
+
+
+        // Todo vary where a player spawns based on playerTeam and hasCannonControl
+        // aswell as vary the player prefab
+        // PlayerInput.Instantiate will done within the switch cases.
+        switch (playerTeam)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+
         PlayerInput playerInput = PlayerInput.Instantiate(playerPrefab[playerIndex], playerIndex, playerControlScheme, -1, playerController);
         playerInput.name = "Player " + (playerIndex + 1).ToString();
         playerInput.tag = "Player " + (playerIndex + 1).ToString();
+
         playerInput.transform.position = new Vector3 (playerSpawns[playerIndex].transform.position.x, playerSpawns[playerIndex].transform.position.y, 0);
         // Activates the player input component on the prefab we just instantiated
         // We have the component disabled by default, otherwise it could not be a "selectable object" independent of the PlayerInput component on the cursor
@@ -79,6 +114,101 @@ public class JumpTheGunLevelInitializer : LevelInitializer
     }
 
 
+    private void AssignPlayerRole(int playerIndex, int teamNumber)
+    {
+
+        var number = Random.Range(1, 3);
+        IsPlayerRoleOccupied(number, playerIndex, teamNumber);
+
+    }
+
+
+    private void IsPlayerRoleOccupied(int randomNumber, int playerIndex, int teamNumber)
+    {
+        bool controlsCannon;
+
+
+        // Random number can either be 1 or 2
+        if (randomNumber == 1)
+            controlsCannon = true;
+        else
+            controlsCannon = false;
+
+
+        // If the player is on the first team and there has not been registered any player to team1Playerdata yet
+        // We add the new JumpTheGunPlayer which contains information regarding if a player controls the cannon,
+        // which team the player is on and the player's index
+        if (teamNumber == 1 && team1PlayerData.Count == 0)
+        {
+            team1PlayerData.Add(new JumpTheGunPlayer(playerIndex, controlsCannon, teamNumber));
+            teamPlayerData.Add(playerIndex, team1PlayerData[0]);
+            return;
+        }
+
+        else if (teamNumber == 2 && team2PlayerData.Count == 0)
+        {
+            team2PlayerData.Add(new JumpTheGunPlayer(playerIndex, controlsCannon, teamNumber));
+            teamPlayerData.Add(playerIndex, team2PlayerData[0]);
+            return;
+        }
+
+
+    // There is a maximum of 4 players allowed in the game atm but it might change in the future
+    // this is the reason why we loop through teamXPlayerData even though there will at max be 1 element in the list
+    // at this point
+        else if (teamNumber == 1 && team1PlayerData.Count != 0)
+        {
+            for (int i = 0; i < team1PlayerData.Count; i++)
+            {
+                if (team1PlayerData[i].controlsCannon == true)
+                {
+                    controlsCannon = false;
+                    team1PlayerData.Add(new JumpTheGunPlayer(playerIndex, controlsCannon, teamNumber));
+                    teamPlayerData.Add(playerIndex, team1PlayerData[1]);
+                }
+            }
+        }
+
+        else if (teamNumber == 2 && team2PlayerData.Count != 0)
+        {
+            for (int i = 0; i < team2PlayerData.Count; i++)
+            {
+                if (team2PlayerData[i].controlsCannon == true)
+                {
+                    controlsCannon = false;
+                    team2PlayerData.Add(new JumpTheGunPlayer(playerIndex, controlsCannon, teamNumber));
+                    teamPlayerData.Add(playerIndex, team2PlayerData[1]);
+                }
+            }
+        }
+    }
+
+
+    private void AssignTeam(int playerIndex)
+    {
+
+        var teamNumber = Random.Range(1, 3);
+        if (team1Counter == 2 && teamNumber == 1)
+            teamNumber = 2;
+        else if (team2Counter == 2 && teamNumber == 2)
+            teamNumber = 1;
+
+        switch (teamNumber)
+        {
+            case 1:
+                AssignPlayerRole(playerIndex, teamNumber);
+                team1Counter++;
+                break;
+            case 2:
+                AssignPlayerRole(playerIndex, teamNumber);
+                team2Counter++;
+                break;
+        }
+
+
+    }
+
+
 // Spawns the UI which holds the scores of the players in the minigame
     private void InstantiatePlayerUI(int playerIndex)
     {
@@ -94,17 +224,34 @@ public class JumpTheGunLevelInitializer : LevelInitializer
         string playerTag = player.tag;
         Destroy(player);
 
-        // Checks in levelRules if this players should respawn in this minigame
-        if (LevelInitializer.levelRules[sceneName]["playersRespawn"])
-            StartCoroutine(RespawnPlayer(respawnTimer, playerToRespawnIndex, playerTag));
+        StartCoroutine(RespawnPlayer(respawnTimer, playerToRespawnIndex));
     }
 
     // Will respawn the player after a short delay
-    public override IEnumerator RespawnPlayer(float seconds, int playerIndex, string playerTag) 
+    public override IEnumerator RespawnPlayer(float seconds, int playerIndex) 
     { 
         yield return new WaitForSeconds(seconds); 
-        SpawnPlayer(playerIndex, playerTag);
+        SpawnPlayer(playerIndex);
     } 
 
 
+}
+
+public class JumpTheGunPlayer 
+{
+    public JumpTheGunPlayer(int pi, bool cc, int pt)
+    {
+        playerIndex = pi;
+        controlsCannon = cc;
+        team = pt;
+    }
+
+    public override string ToString()
+    {
+        return ($"Player {playerIndex + 1}");
+    }
+
+    public bool controlsCannon { get; set; }
+    public int playerIndex { get; set; }
+    public int team { get; set; }
 }
