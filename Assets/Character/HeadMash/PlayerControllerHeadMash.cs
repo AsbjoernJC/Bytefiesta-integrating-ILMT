74aa5aad962fb9c47dc983996960be7e0b87e673
AsbjoernJC <69584571+AsbjoernJC@users.noreply.github.com>
2021-07-22 11:02:00 +0200

    will override the OnJump function in playercontrollerheadmash so canDoubleJump bool will always stay true. Changed canDoubleJump bool to protected from private

--- Assets/Character/HeadMash/PlayerControllerHeadMash.cs
+++ Assets/Character/HeadMash/PlayerControllerHeadMash.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerControllerHeadMash : MonoBehaviour
+public class PlayerControllerHeadMash : PlayerController
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    protected override void Awake() 
     {
-        
+        base.Awake();
+
+        // Player ignore eachother's colissions
+        Physics2D.IgnoreLayerCollision(6, 6, true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnJump(InputAction.CallbackContext context)
     {
+        animator.SetBool("IsJumping", true);
+        if (context.action.triggered)
+        {
+            if (canCoyote)
+            {
+                rB2D.velocity = Vector2.up * m_JumpForce;
+                canCoyote = false;
+                return;
+            }
+
+            if (IsGrounded())
+            {
+                rB2D.velocity = Vector2.up * m_JumpForce;
+                return;
+            }
+            else if (canDoubleJump)
+            {
+                rB2D.velocity = Vector2.up * m_JumpForce;
+                canDoubleJump = false;
+                return;
+            }
+        }
         
+
     }
 }
